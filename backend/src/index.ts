0be77161d86a215b217127ffd784d92c5b73aed3
Ishav <ishavlega@gmail.com>
2024-06-11 08:13:12 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,50 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string
+  }
+}>();
+
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  })
+
+  const token = sign({ id: user.id }, c.env.JWT_SECRET)
+
+  return c.json({
+    jwt: token
+  })
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('signin')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('add blog')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('update blog')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('get blog')
+})
+
+export default app
