ff594bcb4f12b67510d83b8e3080528c56c2ca62
Ishav <ishavlega@gmail.com>
2024-06-13 07:16:46 +0530

    init

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,16 @@
+
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center items-center flex-col">
+        <div>
+            <div className="max-w-lg text-left text-3xl font-bold">
+                "The customer service I received was exeptional. The support team went above and beyond to address my concerns."
+            </div>
+            <div className="max-w-md text-xl font-semibold mt-2">
+                Jules Winnfield
+            </div>
+            <div className="max-w-md text-sm font-light text-slate-400">
+                CEO, Acme corp
+            </div>
+        </div>
+    </div>
+}
